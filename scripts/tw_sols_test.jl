8850673287dc6e8cbeca324fb572f353a9f5c4af
Toby Hammond <tobiasjhammond@gmail.com>
2026-05-25 21:40:17 -0700

    functional

--- scripts/tw_sols_test.jl
+++ scripts/tw_sols_test.jl
@@ -1,31 +1,44 @@
-using Revise
+using Revise, StyledStrings
 
-function gen_eq(k, N)
-    index_matrix = Matrix{Tuple{Int,Int,Int}}(undef, 2N + 1, 2N + 1)
+function print_eq(k, N, flag, n)
+    # creates matrix, populates with 0s
+    m = Matrix{Tuple{Int,Int,Int}}(undef, 2N + 1, 2N + 1)
     for i = 1:2N+1
         for j = 1:2N+1
-            index_matrix[i, j] = (0, 0, 0)
+            m[i, j] = (0, 0, 0)
         end
     end
 
-    for j = max(-2N, -N + k):min(2N, N + k)
+    # computes fhat arguments for each term in sum based on equation (6) from 
+    # notes document
+    # original bounds for j were max(-2N, -N + k):min(2N, N + k) to account for
+    # negative k; we simplify for conciseness
+    for j = -N+k:N+k
         for l = max(-N, -N + j):min(N, N + j)
-            r = j + N + 1
+            r = j + N + 1 - k
             c = l + N + 1
-            index_matrix[r, c] = (k - j, j - l, l)
+            m[r, c] = (k - j, j - l, l)
         end
     end
-    return index_matrix
-end
 
-function print_eq(m, k, N)
+    # graph header and horizontal axis setup
+    println("k = ", k)
+    println("N = ", N)
+    println("n = ", n)
     m_size = 2N + 1
-    print("     ")
+    print("      ")
+
+    # first prints julia matrix indices
     for i = 1:m_size
-        print(i, "  ")
+        print(i, " ")
+        if i < 10
+            print(" ")
+        end
     end
     println()
-    print("    ")
+    print("     ")
+
+    # then prints indices which correspond to the actual j/l index in the sum
     for i = 1:m_size
         print(i - N - 1, " ")
         if i - N >= 0
@@ -33,35 +46,75 @@ function print_eq(m, k, N)
         end
     end
     println()
+
+    # main graph printing
     println("    -------------l------------")
     for i = 1:m_size
+
+        # vertical axis setup
         print(i)
-        if i - N - 1 >= 0
+        if i - N - 1 + k >= 0
             print(" ")
         end
-        print(i - N - 1)
+        if i < 10
+            print(" ")
+        end
+        print(i - N - 1 + k)
         print("|", " ")
-        for j = 1:m_size
-            matches = 0
 
-            if m[i, j][1] == m[i, j][2] && m[i, j][1] == k
-                matches += 1
+        # what the graph outputs: "flag" argument specifies:
+        # 0: print if term contains fhat(index), where index is the desired fhat(k)
+        # 1-3: print the argument for fhat(k - j), fhat(j - l), or fhat (l),
+        # respectively
+        if flag == 0
+            for j = 1:m_size
+                # colors all invalid elements of matrix red
+                matches = 0
+                for x = 1:3
+                    if m[i, j][x] == n
+                        matches += 1
+                    end
+                end
+                if matches != 0
+                    matches_str = AnnotatedString(string(matches), [(1:9, :face, :green)])
+                    print(matches_str, "  ")
+                elseif j - N - 1 < -N + i - N - 1 + k || j - N - 1 > N + i - N - 1 + k
+                    matches_str = AnnotatedString(string(matches), [(1:9, :face, :red)])
+                    print(matches_str, "  ")
+                else
+                    print(matches, "  ")
+                end
             end
-            if m[i, j][1] == m[i, j][3] && m[i, j][1] == k
-                matches += 1
+        else
+            for j = 1:m_size
+                element_str = AnnotatedString(string(m[i, j][flag]), [(1:9, :face, :red)])
+                if j - N - 1 < -N + i - N - 1 + k || j - N - 1 > N + i - N - 1 + k
+                    print(element_str, " ")
+                else
+                    print(m[i, j][flag], " ")
+                end
+                if m[i, j][flag] >= 0 && j < 2N + 1 && m[i, j+1][flag] >= 0
+                    print(" ")
+                end
             end
-            if m[i, j][2] == m[i, j][3] && m[i, j][2] == k
-                matches += 1
-            end
-            print(matches, "  ")
         end
         println()
-        println("   |")
+        println("    |")
     end
 end
-
 k = 1
 N = 4
-m = gen_eq(k, N)
-print_eq(m, k, N)
+flag = 0
+
+# print_eq(k, N, flag, 1)
+# print_eq(k, N, flag, 2)
+# print_eq(k, N, flag, 3)
+# print_eq(k, N, flag, 4)
+
+for k = 1:N
+    println("----------------------------------------------")
+    for n = 1:N
+        print_eq(k, N, 0, n)
+    end
+end
 
