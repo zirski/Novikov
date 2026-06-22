3be483fd006e6cc7eb34746468d2368c8beb52cc
Toby Hammond <tobiasjhammond@gmail.com>
2026-06-22 15:38:00 -0700

    updated to account for all matches

--- scripts/tw_sols_test.jl
+++ scripts/tw_sols_test.jl
@@ -3,8 +3,8 @@ using Revise, StyledStrings
 function print_eq(k, N, flag, n)
     # creates matrix, populates with 0s
     m = Matrix{Tuple{Int,Int,Int}}(undef, 2N + 1, 2N + 1)
-    for i = 1:2N+1
-        for j = 1:2N+1
+    for i = 1:(2N+1)
+        for j = 1:(2N+1)
             m[i, j] = (0, 0, 0)
         end
     end
@@ -13,8 +13,8 @@ function print_eq(k, N, flag, n)
     # notes document
     # original bounds for j were max(-2N, -N + k):min(2N, N + k) to account for
     # negative k; we simplify for conciseness
-    for j = -N+k:N+k
-        for l = max(-N, -N + j):min(N, N + j)
+    for j = (-N+k):(N+k)
+        for l = max(-N, -N+j):min(N, N+j)
             r = j + N + 1 - k
             c = l + N + 1
             m[r, c] = (k - j, j - l, l)
@@ -22,8 +22,8 @@ function print_eq(k, N, flag, n)
     end
 
     # graph header and horizontal axis setup
-    println("k = ", k)
     println("N = ", N)
+    println("k = ", k)
     println("n = ", n)
     m_size = 2N + 1
     print("      ")
@@ -64,17 +64,21 @@ function print_eq(k, N, flag, n)
 
         # what the graph outputs: "flag" argument specifies:
         # 0: print if term contains fhat(index), where index is the desired fhat(k)
-        # 1-3: print the argument for fhat(k - j), fhat(j - l), or fhat (l),
+        # 1 - 3: print the argument for fhat(k - j), fhat(j - l), or fhat (l),
         # respectively
         if flag == 0
             for j = 1:m_size
                 # colors all invalid elements of matrix red
                 matches = 0
                 for x = 1:3
-                    if m[i, j][x] == n
+                    if abs(m[i, j][x]) == n
+                        # if m[i, j][x] == n
+                        # if -m[i, j][x] == n
                         matches += 1
                     end
                 end
+                # if i > 2N + 2 - j
+                #     print("   ")
                 if matches != 0
                     matches_str = AnnotatedString(string(matches), [(1:9, :face, :green)])
                     print(matches_str, "  ")
@@ -103,9 +107,10 @@ function print_eq(k, N, flag, n)
     end
 end
 k = 1
-N = 4
+N = 10
 flag = 0
 
+# print_eq(k, 15, 0, 4)
 # print_eq(k, N, flag, 1)
 # print_eq(k, N, flag, 2)
 # print_eq(k, N, flag, 3)
